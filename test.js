774a421ee80aae42148f36298f92c8a188a36498
YaolinTan <yaolint@nycstudents.net>
2024-10-07 12:20:33 -0400

    e

---
+++ test.js
@@ -0,0 +1,12 @@
+const students = [
+  { name: "Alice", age: 20, grade: 85 },
+  { name: "Bob", age: 22, grade: 90 },
+  { name: "Charlie", age: 21, grade: 78 },
+  { name: "Diana", age: 23, grade: 95 },
+];
+
+// 1. Log each student’s name and grade
+
+//students.forEach((student) => console.log(student.name, student.grade));
+const topStudents = students.filter((student) => student.grade > 80);
+console.log(topStudents);
